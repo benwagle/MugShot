789dc774aad2aad933ea29f12cb09919e06d6788
Mike Jaoudi <mjaoudi93@gmail.com>
2013-09-06 19:06:19 -0400

    Initial Commit

---
+++ Face Finder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Face Finder
+//
+//  Created by Mike Jaoudi on 9/6/13.
+//  Copyright (c) 2013 Mike Jaoudi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Face Finder/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Face Finder
+//
+//  Created by Mike Jaoudi on 9/6/13.
+//  Copyright (c) 2013 Mike Jaoudi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
